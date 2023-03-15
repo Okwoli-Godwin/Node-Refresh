185746fb3291a1178ce7711a2f0ee01268542aaa
Okwoli-Godwin <okwolig60@gmail.com>
2023-03-15 13:57:01 +0100

    added the register controller

--- src/controllers/User.Controller.ts
+++ src/controllers/User.Controller.ts
@@ -1,1 +1,17 @@
-import { Request,Response,NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
+import { IUser } from "../AllInterfaces/User.Interface";
+import UserModel from "../models/User.model";
+import { AppError,HttpCode } from "../Utils/Apperror";
+import { asynHandler } from "../Utils/asynHandler";
+
+export const register = asynHandler(async (req: Request<{},{},IUser>, res: Response, next: NextFunction) => {
+    const { name, email, password, confirmPassword } = req.body;
+    
+    const user = await UserModel.create(email, password, name, confirmPassword);
+        if(!user){
+            next(new AppError({message:"Account Not Created",httpCode:HttpCode}))
+        }
+    return res.status(HttpCode.CREATED).json({
+        message:"Successfully Created User",
+    })
+})
