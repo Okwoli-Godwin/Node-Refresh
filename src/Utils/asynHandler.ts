f568098f80e3a6b7f26bef5bb71134b0c90027c8
Okwoli-Godwin <okwolig60@gmail.com>
2023-03-15 13:17:30 +0100

    added the asyncHandler

---
+++ src/Utils/asynHandler.ts
@@ -0,0 +1,3 @@
+import { Request,Response,NextFunction } from "express";
+
+export const asynHandler = (fn:any)=>(req:Request,res:Response,next:NextFunction)=>Promise.resolve(fn(req,res,next)).catch(next)
